5fd9158cb0654ca06b87b350890a96111d868efd
涂双喜 <759110245@qq.com>
2020-01-15 16:20:59 +0800

    最新

---
+++ app/src/main/java/com/lding/pad/myseial/library/testloading/GuideActivity.java
@@ -0,0 +1,14 @@
+package com.lding.pad.myseial.library.testloading;
+
+
+import com.lding.pad.myseial.R;
+import com.lding.pad.myseial.libding.rerxmvp.base.BaseActivity;
+
+public class GuideActivity extends BaseActivity {
+
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_guide;
+    }
+}
